a98e8a0bf2c42baaf5382dfe0e22d15ae8640928
Virginia Davenport <virginialdport@gmail.com>
2020-05-14 13:33:04 -0600

    button fixed

--- src/App.js
+++ src/App.js
@@ -2,52 +2,50 @@ import React,{useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
 import {API_WITH_KEY} from './constants'
-import Button from './components/Button'
+
 
 function App() {
-const [image, setImage] = useState([])
-const [info, setInfo] = useState(null)
+  const [image, setImage] = useState([])
+  const [toggleInfo, setToggleInfo] = useState(false)
 
+  const toggleInfoHandler = event => {
+    event.preventDefault()
+    setToggleInfo(!toggleInfo)
+  }
 
-const openInfo = e => {
-  setInfo(e)
-}
-
-const closeInfo = () => {
-  setInfo(null)
-}
-  // useEffect(() => {
-  //   axios.get(`${API_WITH_KEY}`)
-  //     .then(result => {
-  //       setImage(result.data)
-  //     })
-  // },[])
- const Image = (props) => {
-   return (
-     <div className='image'>
-       {/* {props.image.title}  */}
-       <button onClick={() => openInfo(props.image)}>
-         More Info
-       </button>
-     </div>
-   )
- }
+    useEffect(() => {
+      axios.get(`${API_WITH_KEY}`)
+        .then(result => {
+          setImage(result.data)
+        })
+    },[])
+ 
   return (
     <div className="App">
       <header>
-        <h1>Nasa Picture of the day: image.date</h1>
+        <h1>Nasa Picture of the day: {image.date}</h1>
       </header>
-      {/* <img src={image.url} alt='nasa image of the day'/> */}
-      <Image />
-      {
-        image && <Button currentimage={image} close={closeInfo}
-      }
-      <Button/>
-      {/* <button onClick={() => openInfo{image.data}}>
-        More Info
-      </button> */}
+      <main>
+        <img src={image.url} alt="nasa of the day" />
+        <button onClick={e => {toggleInfoHandler(e)}}>
+          {
+            toggleInfo ? 'collapse':'expand'
+          }
+        </button>
+        {
+          toggleInfo && (
+            <>
+              <h2>{image.title}</h2>
+              <p>{image.explanation}</p>
+            </>
+          )
+        }
+      </main>
+      
+     
+    
     </div>
-   
+    
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../App.css'
 import axios from 'axios'
 import {API_WITH_KEY} from '../constants'
 
 const Button=(props)=>{
-    
+    const[details, setdetails] = useState(null)
 
-    useEffect
+    useEffect(() => {
+        // axios.get(${API_WITH_KEY
+        //     .then(result => {
+        //         setdetails(result.data)
+        //     })
+        // })
+    },[image])
+    
+    if (!details){
+        return `No details for ${image}`
+    }
 
     return(
         <div className="button-info">
-            <h2>props.image.title</h2>
-            <p>oeahvhareihvaihifoedrafioh</p>
+            <h2>details.title</h2>
+            <p>details.explanation</p>
         </div>
     )
 }
